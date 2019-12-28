3c96d36686e2d8f73c8f5149269542a87beda422
Lucas Savva <lucas@m1cr0man.com>
2019-12-28 17:47:57 +0000

    oauth2: Use channels instead of a dedi web server

--- pkg/oauth2/api.go
+++ pkg/oauth2/api.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pkg/browser"
 	"golang.org/x/oauth2"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -17,7 +18,14 @@ import (
 
 type API struct {
 	authConfig *oauth2.Config
+	AuthUrl    string
+	Username   string
 	Client     *http.Client
+}
+
+type TokenWithUsername struct {
+	Token    *oauth2.Token `json:"token"`
+	Username string        `json:"username,omitempty"`
 }
 
 func (i *API) AuthFromWeb() (*oauth2.Token, error) {
@@ -39,73 +47,61 @@ func (i *API) AuthFromWeb() (*oauth2.Token, error) {
 		fmt.Println("Authorisation page opened. Check your browser.")
 	}
 
-	// Start a HTTP server to handle the oauth response from the client browser
-	server := &http.Server{Addr: ":8099"}
-
 	// Decode CallbackPage
 	// It will never fail, it's hard coded
 	CallbackPageParsed, _ := base64.StdEncoding.DecodeString(CallbackPage)
+	tokenChannel := make(chan oauth2.Token)
 
-	var token *oauth2.Token
-
-	http.HandleFunc("/oauthcallback", func(resp http.ResponseWriter, req *http.Request) {
+	http.HandleFunc(i.AuthUrl, func(resp http.ResponseWriter, req *http.Request) {
 		if req.Method == http.MethodGet {
 			resp.Header().Set("Content-Type", "text/html;charset=UTF-8")
 			resp.Header().Set("Content-Encoding", "gzip")
 
 			// Check state
 			if req.URL.Query().Get("state") != stateStr {
-				fmt.Println("STATE STRING DOES NOT MATCH! You have probably been hacked." +
+				fmt.Println("STATE STRING DOES NOT MATCH! You have probably been hacked. " +
 					"Check your system security and try again")
-
-				// Shutdown soon
-				go func() {
-					time.Sleep(time.Second)
-					_ = server.Shutdown(context.Background())
-				}()
-
+				resp.WriteHeader(401)
+				close(tokenChannel)
 				return
 			}
 
 			if _, err := resp.Write(CallbackPageParsed); err != nil {
 				fmt.Println("Failed to write GET response: ", err)
+				close(tokenChannel)
 			}
 
 		} else if req.Method == http.MethodPost {
 
 			if err := req.ParseForm(); err != nil {
 				fmt.Println("Failed to parse POST form data: ", err)
+				close(tokenChannel)
 				return
 			}
 
 			// Parse form data to an Oauth2 token
 			expiry, _ := strconv.Atoi(req.FormValue("expires_in"))
-			token = &oauth2.Token{
+			tokenChannel <- oauth2.Token{
 				Expiry:       time.Now().Add(time.Second * time.Duration(expiry)),
 				TokenType:    req.FormValue("token_type"),
 				AccessToken:  req.FormValue("access_token"),
 				RefreshToken: req.FormValue("refresh_token"),
 			}
 
+			close(tokenChannel)
 			resp.WriteHeader(201)
-
-			// Shutdown soon
-			go func() {
-				time.Sleep(time.Second)
-				_ = server.Shutdown(context.Background())
-			}()
 		}
 	})
 
-	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		return nil, err
+	token, ok := <-tokenChannel
+	if !ok {
+		return nil, fmt.Errorf("failed to get token")
 	}
 
-	return token, nil
+	return &token, nil
 }
 
-func (i *API) AuthFromFile(tokenFile string) (token *oauth2.Token, err error) {
-	token = &oauth2.Token{}
+func (i *API) AuthFromFile(tokenFile string) (token *TokenWithUsername, err error) {
 
 	// Try reading the token from the file
 	tokenData, err := ioutil.ReadFile(tokenFile)
@@ -116,7 +112,7 @@ func (i *API) AuthFromFile(tokenFile string) (token *oauth2.Token, err error) {
 	return
 }
 
-func (i *API) SaveAuth(tokenFile string, token *oauth2.Token) error {
+func (i *API) SaveAuth(tokenFile string, token *TokenWithUsername) error {
 	jsonData, _ := json.Marshal(token)
 	return ioutil.WriteFile(tokenFile, jsonData, 0640)
 }
@@ -126,22 +122,41 @@ func (i *API) SetConfig(config *oauth2.Config) {
 }
 
 func (i *API) Authorize(tokenFile string) error {
-	var token *oauth2.Token
-
 	token, err := i.AuthFromFile(tokenFile)
 
 	// Don't complain, just do web auth
-	if err != nil {
+	if err != nil || !token.Token.Valid() {
 		err = nil
-		token, err = i.AuthFromWeb()
+		newToken, err := i.AuthFromWeb()
+
+		if err != nil {
+			return fmt.Errorf("failed to get authorisation token from Imgur: %s", err)
+		}
+		token = &TokenWithUsername{
+			newToken,
+			"",
+		}
 	}
 
-	if err != nil {
-		return fmt.Errorf("failed to read authorisation token: %s", err)
+	// Force a token refresh so that we can get the username
+	if token.Username == "" {
+		token.Token.Expiry = time.Now().Add(-time.Hour)
 	}
+	source := i.authConfig.TokenSource(context.Background(), token.Token)
+	newToken, err := source.Token()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	token.Token = newToken
 
 	// Set up authenticated http client
-	i.Client = i.authConfig.Client(context.Background(), token)
+	i.Client = oauth2.NewClient(context.Background(), source)
+
+	// If token was refreshed set the username
+	if newUsername, ok := newToken.Extra("account_username").(string); ok {
+		token.Username = newUsername
+	}
+	i.Username = token.Username
 
 	// Save the token
 	if err = i.SaveAuth(tokenFile, token); err != nil {
@@ -151,4 +166,10 @@ func (i *API) Authorize(tokenFile string) error {
 	fmt.Println("Token received!")
 
 	return nil
+}
+
+func NewAPI(authUrl string) *API {
+	return &API{
+		AuthUrl: authUrl,
+	}
 }
